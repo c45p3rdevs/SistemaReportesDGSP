7eed5fced4760566ed52ddd84f140b8436393176
Your Name <you@example.com>
2024-09-09 18:10:26 -0600

    RepararTablas en exel

--- my-app/controllers/funciones_home.py
+++ my-app/controllers/funciones_home.py
@@ -246,10 +246,22 @@ def buscarEmpleadoBD(search):
                             e.nombre_empleado, 
                             e.apellido_empleado,
                             e.salario_empleado,
-                            CASE
-                                WHEN e.sexo_empleado = 1 THEN 'Masculino'
-                                ELSE 'Femenino'
-                            END AS sexo_empleado
+                           CASE
+                            WHEN e.sexo_empleado = 1 THEN 'Acambaro' 
+                            WHEN e.sexo_empleado = 2 THEN 'Celaya' 
+                            WHEN e.sexo_empleado = 3 THEN 'Guanajuato' 
+                            WHEN e.sexo_empleado = 4 THEN 'Irapuato' 
+                            WHEN e.sexo_empleado = 5 THEN 'Leon' 
+                            WHEN e.sexo_empleado = 6 THEN 'Penjamo' 
+                            WHEN e.sexo_empleado = 7 THEN 'Salamanca' 
+                            WHEN e.sexo_empleado = 8 THEN 'San Felipe' 
+                            WHEN e.sexo_empleado = 9 THEN 'San Miguel' 
+                            WHEN e.sexo_empleado = 10 THEN 'Valle1' 
+                            WHEN e.sexo_empleado = 11 THEN 'Valle2' 
+                            WHEN e.sexo_empleado = 12 THEN 'Valle3' 
+                            
+                            
+                            ELSE 'Acambaro'  
                         FROM tbl_empleados AS e
                         WHERE e.nombre_empleado LIKE %s 
                         ORDER BY e.id_empleado DESC
